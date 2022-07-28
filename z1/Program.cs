3af6c78ab7d41509cb8b614b1e5037813d2110d2
EgorEvtushenko <ljvbyj0990@gmail.com>
2022-07-29 00:27:45 +0500

    Homework seminar 10

---
+++ z1/Program.cs
@@ -0,0 +1,19 @@
+﻿int CountVowel (string[] array)
+{
+    int count = 0;
+
+    for(int i = 0; i < array.Length; i++)
+    {
+        if(array[i][0] == 'a' || array[i][0] == 'e' 
+        || array[i][0] == 'i' || array[i][0] == 'o' 
+        || array[i][0] == 'u' || array[i][0] == 'y')
+        {
+            count++;
+        }
+    }
+
+    return count;
+}
+
+string[] words = {"asd", "wad", "efg", "qwe"};
+Console.Write(CountVowel(words));
---
+++ z2/Program.cs
@@ -0,0 +1,22 @@
+﻿string[] ConcatinationArray(string[] array)
+{
+    string[] ConcArray = new string[array.Length];
+
+    for(int i = 0; i < array.Length; i = i + 2)
+    {
+        ConcArray[i] = array[i] + array[i+1];
+    }
+
+    return ConcArray;
+}
+
+void ShowStringArray(string[] array)
+{
+    for(int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+}
+
+string[] words = {"asd", "wad", "efg", "qwe", "dfg", "ret", "ops", "vbn"};
+ShowStringArray((ConcatinationArray(words)));
